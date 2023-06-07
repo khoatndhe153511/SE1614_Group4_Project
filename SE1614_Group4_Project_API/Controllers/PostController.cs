453bd54cd14041bfbf8fb2cac420892493a073c0
quangthai <quangthai05122001@gmail.com>
2023-06-07 14:47:56 +0700

    update

--- SE1614_Group4_Project_API/Controllers/PostController.cs
+++ SE1614_Group4_Project_API/Controllers/PostController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SE1614_Group4_Project_API.Models;
-using SE1614_Group4_Project_API.Repository;
 using SE1614_Group4_Project_API.Repository.Interfaces;
 
 namespace SE1614_Group4_Project_API.Controllers
@@ -12,7 +11,7 @@ namespace SE1614_Group4_Project_API.Controllers
     {
         private readonly IRepository<Post> _postRepository;
 
-        public PostController(Repository<Post> postRepository)
+        public PostController(IRepository<Post> postRepository)
         {
             _postRepository = postRepository;
         }
