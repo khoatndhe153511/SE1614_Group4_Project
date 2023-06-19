64b56f45fcad061b39c77199b7c3da65bb570c99
quangthai <quangthai05122001@gmail.com>
2023-06-19 22:11:19 +0700

    refactor auth controller

--- SE1614_Group4_Project_API/Controllers/Auth/AuthController.cs
+++ SE1614_Group4_Project_API/Controllers/Auth/AuthController.cs
@@ -1,14 +1,12 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Authentication;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using SE1614_Group4_Project_API.DTOs;
 using SE1614_Group4_Project_API.Models;
-using SE1614_Group4_Project_API.Repository.Interfaces;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace SE1614_Group4_Project_API.Controllers.Auth
 {
@@ -20,8 +18,7 @@ namespace SE1614_Group4_Project_API.Controllers.Auth
         private readonly IConfiguration _config;
         private readonly IMapper _mapper;
 
-        public AuthController(spriderumContext context, IConfiguration config, IMapper mapper,
-            IUserRepository userRepository)
+        public AuthController(spriderumContext context, IConfiguration config, IMapper mapper)
         {
             _context = context;
             _config = config;
@@ -34,11 +31,10 @@ namespace SE1614_Group4_Project_API.Controllers.Auth
             try
             {
                 var user = _context.Users
-                            .Where(x => x.Email.Equals(loginModel.Email) && x.Password.Equals(loginModel.Password))
-                            .FirstOrDefault();
+                    .FirstOrDefault(x => x.Email.Equals(loginModel.Email) && x.Password.Equals(loginModel.Password));
                 if (user != null)
                 {
-                    var tokenStr = GenerateJSONWebToken(user);
+                    var tokenStr = GenerateJsonWebToken(user);
                     Response.Cookies.Append("ACCESS_TOKEN", tokenStr);
                     return Ok(new { token = tokenStr });
                 }
@@ -72,32 +68,28 @@ namespace SE1614_Group4_Project_API.Controllers.Auth
         public ActionResult<User> SignUp([FromBody] UserRegisterDto newUser)
         {
             var existedUser = _context.Users
-                .Where(x => x.Email.Equals(newUser.Email) && x.Name.Equals(newUser.UserName))
-                .FirstOrDefault();
+                .FirstOrDefault(x => x.Email.Equals(newUser.Email) && x.Name.Equals(newUser.UserName));
 
-            if (existedUser == null)
+            if (existedUser != null) return BadRequest("Username or email already taken");
+            if (newUser.Password != newUser.ConfirmPassword)
             {
-                if (newUser.Password != newUser.ConfirmPassword)
-                {
-                    return BadRequest("Confirm Password Failed!");
-                }
-
-                try
-                {
-                    var newMem = _mapper.Map<User>(newUser);
-                    _context.Users.Add(newMem);
-                    _context.SaveChanges();
-                    return Ok(newMem);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
+                return BadRequest("Confirm Password Failed!");
             }
-            return BadRequest("Username or email already taken");
+
+            try
+            {
+                var newMem = _mapper.Map<User>(newUser);
+                _context.Users.Add(newMem);
+                _context.SaveChanges();
+                return Ok(newMem);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        private string GenerateJSONWebToken(User userInfo)
+        private string GenerateJsonWebToken(User userInfo)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
@@ -111,11 +103,11 @@ namespace SE1614_Group4_Project_API.Controllers.Auth
             };
 
             var token = new JwtSecurityToken(
-                    issuer: _config["Jwt:Issuer"],
-                    audience: _config["Jwt:Issuer"],
-                    claims,
-                    expires: DateTime.UtcNow.AddMinutes(120),
-                    signingCredentials: credentials);
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(120),
+                signingCredentials: credentials);
 
             var encodeToken = new JwtSecurityTokenHandler().WriteToken(token);
             return encodeToken.ToString();
