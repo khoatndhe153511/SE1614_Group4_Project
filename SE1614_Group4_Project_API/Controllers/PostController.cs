973b7bd539d7bcc65acd1c1aa51419a693e3995b
Trần Nguyễn Đăng Khoa <khoatnd@kaopiz.com>
2023-06-21 09:29:07 +0700

    Post list

--- SE1614_Group4_Project_API/Controllers/PostController.cs
+++ SE1614_Group4_Project_API/Controllers/PostController.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using SE1614_Group4_Project_API.Models;
 using SE1614_Group4_Project_API.Repository;
 using SE1614_Group4_Project_API.Repository.Interfaces;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace SE1614_Group4_Project_API.Controllers
 {
@@ -11,17 +13,34 @@ namespace SE1614_Group4_Project_API.Controllers
     public class PostController : Controller
     {
 
-		private readonly IPostRepository _postRepository;
+        private readonly IPostRepository _postRepository;
+        private readonly spriderumContext _spriderumContext;
 
-		public PostController(IPostRepository postRepository)
+        public PostController(IPostRepository postRepository,spriderumContext spriderumContext)
         {
+            _spriderumContext = spriderumContext;
             _postRepository = postRepository;
         }
 
         [HttpGet]
-        public IActionResult GetAllPost()
+        public IActionResult GetAllPost(int page, int pageSize)
         {
-            return Ok(_postRepository.GetAll());
+            var posts = _spriderumContext.Posts.Where(_ => _.IsEditorPick == null).Select(_ => new
+            {
+                id = _.Id,
+                Created = _.CreatedAt,
+                Modified = _.ModifiedAt,
+                Title = _.Title,
+                CategoryName = _.Cat.Name,
+                AuthorName = _.Creator.Name
+            }).ToList();
+
+            var totalPosts = posts.Count;
+            var totalPages = (int)Math.Ceiling((double)totalPosts / pageSize);
+
+            var pagedPosts = posts.Skip((page - 1) * pageSize).Take(pageSize);
+
+            return Ok(new { Posts = pagedPosts, TotalPosts = totalPosts, TotalPages = totalPages });
         }
 
         [HttpGet("{pid}")]
