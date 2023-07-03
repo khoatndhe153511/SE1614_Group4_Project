43c0b73a3ad96b90aeea4fc1fd70e317a5e663f8
quangthai <quangthai05122001@gmail.com>
2023-07-03 16:37:03 +0700

    update

--- SE1614_Group4_Project_API/Models/spriderumContext.cs
+++ SE1614_Group4_Project_API/Models/spriderumContext.cs
@@ -35,8 +35,7 @@ namespace SE1614_Group4_Project_API.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=spriderum;user=sa;password=123;TrustServerCertificate=true");
+                // optionsBuilder.UseSqlServer("Server=localhost;Database=spriderum;user=sa;password=123;TrustServerCertificate=true");
             }
         }
 
